1a380ebf75e92218be3078ef051249f9f61f39b6
Zhuo Liu <vin_lz@outlook.com>
2018-10-30 22:06:28 -0400

    feat: add funcion to shuffle and open cards

---
+++ memory-game/js/app.js
@@ -0,0 +1,90 @@
+//global variables
+let cardTypes = ['fa-diamond', 'fa-diamond',
+               'fa-paper-plane-o', 'fa-paper-plane-o',
+               'fa-anchor', 'fa-anchor',
+               'fa-bolt', 'fa-bolt',
+               'fa-cube', 'fa-cube',
+               'fa-leaf', 'fa-leaf',
+               'fa-bicycle', 'fa-bicycle',
+               'fa-bomb', 'fa-bomb'];
+
+let moves = 0;
+
+let cards;
+let openCards = [];
+
+
+
+
+
+
+/*
+ * Create a list that holds all of your cards
+ */
+function createCard(cardType) {
+	return `<li class="card"><i class="fa ${cardType}"></i></li>`;
+}
+
+function creatCardListHTML() {
+	let cardHTML = cardTypes.map(function(card) {
+		return createCard(card);
+	});
+	return cardHTML.join(' ');
+}
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided 'shuffle' method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+function initGame() {
+	cardTypes = shuffle(cardTypes);
+	let deck = document.querySelector('.deck');
+	deck.innerHTML = creatCardListHTML();
+}
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+function handleClick(event, card) {
+
+}
+
+initGame();
+
+cards = document.querySelectorAll('.card');
+for (let card of cards) {
+	card.addEventListener('click', function(event) {
+		if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
+			card.classList.add('open', 'show');
+			console.log("opened");
+			openCards.push(card);
+		}
+	});
+}
+
+
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of 'open' cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
